22d2f6dd51f9f01be8ac15b94345fa2c07ba3716
Lucas Santos Rodrigues <70177902+LuSrodri@users.noreply.github.com>
2022-11-08 21:40:04 -0300

    atualizando o Footer e a Home

--- my-project/src/Components/Footer.js
+++ my-project/src/Components/Footer.js
@@ -1,17 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './Footer.css';
 
 const divStyle = {
     backgroundColor: '#1C3144',
     color: '#F3D8C7',
     paddingTop: '5vmin',
     paddingBottom: '2.5vmin',
+    borderTop: '1vmin solid #EF626C',
 };
+
 
 export function Footer() {
     return (
         <div className="home w-100" style={divStyle}>
-            <h4>Me siga nas redes sociais!</h4>
+            <h4>Siga-me nas redes sociais!</h4>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <a href="https://www.linkedin.com/in/lucas-santos-rodrigues/"><i class="bi bi-linkedin icon"></i></a>
+                <a href="https://github.com/lusrodri"><i class="bi bi-github icon"></i></a>
+            </div>
         </div>
     );
 }
--- my-project/src/Components/Home.js
+++ my-project/src/Components/Home.js
@@ -1,25 +1,46 @@
-import React, {Component} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Typed from 'react-typed';
 
 const divStyle = {
     backgroundColor: '#1C3144',
     color: '#F3D8C7',
     paddingTop: '5vmin',
     paddingBottom: '2.5vmin',
+    borderBottom: '1vmin solid #EF626C',
 };
 
 const titleStyle = {
-    marginTop: '2.5vh',
-    marginBottom: '5vh',
     fontSize: '8vmin',
+    color: '#EF626C',
+    fontWeight: 'bolder',
 };
 
-export function Home() {
-    return (
-        <div className="home w-100" style={divStyle}>
-            <h1 style={titleStyle}>LuSrodri.me</h1>
-            <h4>Eu sou Lucas Santos Rodrigues, desenvolvedor WEB e esse é o meu portfolio!😊</h4>
-            <h2></h2>
-        </div>
-    );
+let index = 0;
+
+export class Home extends React.Component {
+    technologies = ['WEB', 'HTML', 'CSS', 'JavaScript', 'Node.js', 'React.js', 'ASP.NET'];
+
+    constructor(props) {
+        super(props);
+        this.state = { technology: 'Front-end_' };
+    }
+
+    render() {
+        return (
+            <div className="home w-100" style={divStyle}>
+                <h1 style={titleStyle}>LUSRODRI</h1>
+                <h4>Eu sou Lucas Santos Rodrigues, desenvolvedor <Typed
+                    typedRef={(typed) => { this.typed = typed; }}
+                    strings={this.technologies}
+                    typeSpeed={75}
+                    backSpeed={60}
+                    cursorChar="_"
+                    loop
+                    style={{fontWeight: 'bolder'}}
+                    /></h4>
+                <h4>Seja bem-vindo ao meu portfolio.</h4>
+            </div>
+        );
+    }
 }
