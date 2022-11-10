5663042292abe72ea3ae8feb55ae1759c0e9c78d
Lucas Santos Rodrigues <70177902+LuSrodri@users.noreply.github.com>
2022-11-10 16:09:00 -0300

    alteração do componente Projects com a inserção do Projects.json e definição do componente Card

---
+++ my-project/src/Components/Card.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import './Card.css';
+
+export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tagsElement: '',
+            type: '',
+        };
+        for (let i = 0; i < this.props.tags.length; i++) {
+            if (i >= 6) {
+                this.state.tagsElement = this.state.tagsElement += '...';
+                break;
+            }
+
+            if (i === this.props.tags.length - 1)
+                this.state.tagsElement = this.state.tagsElement += this.props.tags[i] + '.';
+            else
+                this.state.tagsElement = this.state.tagsElement += this.props.tags[i] + ', ';
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.type == 'site')
+            this.setState({ type: 'WebSite' });
+        else if (this.props.type == 'repo')
+            this.setState({ type: 'Repositório' });
+    }
+
+    render() {
+
+        if (this.props.img == '')
+            return (
+                <div className="divCard">
+                    <div>
+                        <h2>{this.props.title}</h2>
+                        <p>{this.state.tagsElement}</p>
+                    </div>
+                    <h4>Clique e veja mais sobre esse {this.state.type}!</h4>
+                </div>
+            );
+        else
+            return (
+                <div className="divCard">
+                    <img src={this.props.img} />
+                    <div>
+                        <h2>{this.props.title}</h2>
+                        <p>{this.state.tagsElement}</p>
+                    </div>
+                </div>
+            );
+
+    }
+}
--- my-project/src/Components/Projects.js
+++ my-project/src/Components/Projects.js
@@ -1,5 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Card } from './Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './Data/projects.json';
 
 const divStyle = {
     color: 'black',
@@ -7,10 +9,40 @@ const divStyle = {
     paddingBottom: '2.5vmin',
 };
 
-export function Projects() {
-    return (
-        <div className="home w-100" style={divStyle}>
-            <h3 style={{fontWeight: 'bolder', color: '#1C3144'}}>Projetos:</h3>
-        </div>
-    );
+const cardsStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    margin: '3vmin 0',
+    width: '90%',
+};
+
+export class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: data.projects,
+            cards: [],
+        };
+        for (let i = 0; i < this.state.projects.length; i++) {
+            this.state.cards.push(
+                    <Card key={i}
+                        img={this.state.projects[i].image}
+                        title={this.state.projects[i].name}
+                        tags={this.state.projects[i].tags}
+                        type={this.state.projects[i].type} />)
+        }
+    }
+
+    render() {
+        return (
+            <div className="home w-100" style={divStyle}>
+                <h1 style={{ fontWeight: 'bolder', color: '#1C3144' }}>PROJETOS</h1>
+                <div style={cardsStyle}>
+                    {this.state.cards}
+                </div>
+            </div>
+        )
+    }
 }
