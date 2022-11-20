d21d2d6e15223808ed8384be3f5a0b9dfcadf042
Lucas Santos Rodrigues <70177902+LuSrodri@users.noreply.github.com>
2022-11-20 16:04:12 -0300

    removendo o react-typed

--- my-project/src/Components/Home.js
+++ my-project/src/Components/Home.js
@@ -1,24 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Typed from 'react-typed';
 import './Styles/Home.css';
 
 export class Home extends React.Component {
-    technologies = ['WEB', 'HTML', 'CSS', 'JavaScript', 'Node.JS', 'React.JS', 'ASP.NET'];
-
     render() {
         return (
             <div className="divHome">
                 <h1 className="title">LUSRODRI</h1>
-                <h4 className="text">Eu sou Lucas Santos Rodrigues, desenvolvedor <Typed
-                    typedRef={(typed) => { this.typed = typed; }}
-                    strings={this.technologies}
-                    typeSpeed={75}
-                    backSpeed={60}
-                    cursorChar="_"
-                    loop
-                    style={{fontWeight: 'bolder'}}
-                    /></h4>
+                <h4 className="text">Eu sou Lucas Santos Rodrigues, desenvolvedor WEB.</h4>
                 <h4 className="text">Seja bem-vindo ao meu portfólio.</h4>
             </div>
         );
